9e3e847ad43193e9683227dcdb4ab8f36292e39d
SmerkyG <SmerkyG@users.noreply.github.com>
2024-09-20 14:24:08 -0400

    dev

--- models/qwen2.py
+++ models/qwen2.py
@@ -595,9 +595,9 @@ class Model_qwen2(nn.Module): # Qwen2CausalLM
         #     decoder_layer.post_attention_layernorm.requires_grad_(False)
         #     for p in decoder_layer.mlp.parameters():
         #         decoder_layer.mlp.requires_grad_(False)
-        self.model.embed_tokens.requires_grad_(False)
+        # self.model.embed_tokens.requires_grad_(False)
         self.model.norm.requires_grad_(False)
-        self.lm_head.requires_grad_(False)
+        # self.lm_head.requires_grad_(False)
 
         # JIT at last minute
         for decoder_layer in self.model.layers:
